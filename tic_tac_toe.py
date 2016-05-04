45becb99b514f3c963abfac1249d34a4941026af
Jean <juanpedro1519@gmail.com>
2016-05-04 20:18:23 -0300

    Board Structure

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,11 +2,29 @@ import pygame
 
 pygame.init()
 
+def draw_lines(pantalla) :
+
+	x = 200
+	yinicio = 100
+	yfinal = 400 
+
+	for i in range(0,2) :
+
+		pygame.draw.line(pantalla, (0,0,0),(x,yinicio),(x,yfinal),1)
+		x += 100
+
+	xyfinal = 200
+	for i in range(0,2) :
+
+		pygame.draw.line(pantalla, (0,0,0),(100,xyfinal),(400,xyfinal),1)
+		xyfinal += 100
+		
+
 def main() :
 
 	pygame.init()
 
-	pantalla = pygame.display.set_mode([500,500])
+	pantalla = pygame.display.set_mode([700,500])
 
 	reloj = pygame.time.Clock()
 	salir = False
@@ -21,6 +39,8 @@ def main() :
 
 		reloj.tick(20)
 		pantalla.fill((255,255,255))
+
+		draw_lines(pantalla)
 		pygame.display.update()
 
 	pygame.quit()
